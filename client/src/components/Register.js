d58cb59f017f5b08140b74dc35fe8b761c72c5f9
Alanoterohs <alanoterohs@gmail.com>
2021-08-30 08:43:04 -0300

    estructura de componente 'registro'

---
+++ client/src/components/Register.js
@@ -0,0 +1,28 @@
+function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <form>
+        <input
+        placeholder = 'name'
+        />
+        <input
+        placeholder = 'last name'
+        />
+        <input
+        placeholder = 'email'
+        />
+        <input
+        placeholder = 'password'
+        />
+        <input
+        placeholder = 'repeat password'
+        />
+        <button>Register</button>
+        <button>Have an account</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
