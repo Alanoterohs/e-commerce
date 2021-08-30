1869de2345364c4c9f820ee35bf885dacaef3bb1
Alanoterohs <alanoterohs@gmail.com>
2021-08-30 10:43:41 -0300

    estilo al Login con Bulma
    adhiero la dependencia de Bulma como framework de css y le doy estilo al login

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,28 +1,62 @@
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-      <form>
-        <input
-        placeholder = 'name'
-        />
-        <input
-        placeholder = 'last name'
-        />
-        <input
-        placeholder = 'email'
-        />
-        <input
-        placeholder = 'password'
-        />
-        <input
-        placeholder = 'repeat password'
-        />
-        <button>Register</button>
-        <button>Have an account</button>
-      </form>
-    </div>
+        <section className="hero is-fullheight is-link">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns is-centered has-text-centered">
+                <div className="column is-5-tablet is-4-desktop is-5-widescreen">
+                  <h3 className="title has-text-white">Login</h3>
+                    <hr className="login-hr"/>
+                  <form>
+                  <div className="field is-horizontal is-align-items-center">
+                    <div className="field-body">
+                      <div className="field">
+                          <input className="input" type="email"/>
+                      </div>
+                    </div>
+                  </div>
+                    <div className="field">
+                      <div className="control">
+                        <input className="input is-normal" type="email" placeholder="Last name" autoFocus=""/>
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="control">
+                        <input className="input is-normal" type="email" placeholder="Email" autoFocus=""/>
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="control">
+                        <input className="input is-normal" type="password" placeholder="Password"/>
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="control">
+                        <input className="input is-normal" type="password" placeholder="Repeat password"/>
+                      </div>
+                    </div>
+                    <div className="field is-grouped is-justify-content-center">
+                      <div className="control">
+                        <button className="button is-primary">Submit</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
   );
 }
+// <div className="hero-body">
+// <div className="container has-text-centered">
+// <p className="title">
+// Title
+// </p>
+// <p className="subtitle">
+// Subtitle
+// </p>
+// </div>
+// </div>
 
 export default Register;
